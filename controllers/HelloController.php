f920d9a358850c30fb6f2e3ba69cebfc4cc1f37f
Storozhuk Nikita <storozhuk.nikita@gmail.com>
2019-11-18 21:02:33 +0300

    Finish 1 lesson

---
+++ controllers/HelloController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace app\controllers;
+
+use yii\web\Controller;
+
+class HelloController extends Controller
+{
+
+    /**
+     * @return string
+     */
+    public function actionWorld()
+    {
+        return $this->render('world');
+    }
+
+}
